309542615a32549ca8244d0252412b4a5d2321d6
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-19 15:30:40 -0300

    Challenge doc swagger completely solved

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -12,7 +12,6 @@ class ListAllUsersController {
       const users = this.listAllUsersUseCase.execute({
         user_id: String(user_id),
       });
-
       return response.status(200).json(users);
     } catch (error) {
       return response.status(400).json({ error: error.message });
